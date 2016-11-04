ccec2825547e9a5a754ecde06e26333161bc127a
MarkIsNotTaken <mdemesillo@gmail.com>
2016-11-04 12:34:16 -1000

    app.js

---
+++ app.js
@@ -0,0 +1,79 @@
+var hero = "Florida Man";
+var gender = "Man";
+var age = 462;
+var weapon = "live crocodile";
+var pronoun;
+var xp;
+
+function gender(){
+	if(gender = "Man"){pronoun = "His"}
+}
+
+function experience() {
+	if(age < 18){xp = "is just beginning"}
+		else if(19 > age < 40){xp = "has seen a few battles"}
+			else if(age > 40){xp = "has live through many epic battles"}
+}
+
+console.log(Hero + " is our lovable hero, who " + xp + " with his trusty " + weapon)
+
+var count;
+var animal = "Tasmanian Gerbil";
+
+function animalHouse(a,b){
+	{count = a + b}{
+	animalHouse(1,2)
+	}
+}
+
+function check(){
+	if(count > 2){
+		console.log("We have too many " + animal + "s")
+	}else{
+		console.log("Welcome to the club")
+	}
+}
+
+var secondHero = "Hulk";
+
+function avengersAssemble(avenger){
+	if(avenger ==  "Hulk"){
+		console.log(avenger + "want friend")
+	}else("Sorry, you are not needed for this mission")
+}
+
+avengersAssemble("Hulk");
+
+var lights = true;
+
+function luminus(){
+	if(lights = true){
+		console.log("And then there was light")
+	}else{"Lights out"}
+}
+
+// create a variable named carType and set the value to Toyota, Lamborghini, or Tesla as a string. 
+// create a variable named color and set the value to red, blue, or black as a string 
+// create a variable named num and set the value to an integer
+//create a conditional and check for the following: 
+// check for a blue Toyota 
+// check for a red Lamborghini 
+// check for a black Tesla 
+//console log "We have" and your variables. for each condition. 
+// if none of the conditions are true console log this sentence with your variables,"We don't have any blue Toyotas in stock"
+
+var carType = "Toyota";
+var color = "Black";
+var num = 2;
+
+function(){
+	if(carType == Toyota, color == blue){
+		console.log("We have " + color + " " + carType)
+	}if(carType == Lamborghini, color == red){
+		console.log("We have " + color + " " + carType)
+	}if(carType == Tesla, color == black){
+		console.log("We have " + color + " " + carType)
+	}else{
+		console.log("We don't have any " + color + " " + carType + "s in stock")
+	}
+}
